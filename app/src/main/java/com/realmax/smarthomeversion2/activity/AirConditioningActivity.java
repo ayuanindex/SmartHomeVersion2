ab7c9d6fe047f7d68af4372b78d85974e0c88e9f
AYuan <lzy2118053626@hotmaill.com>
2020-04-08 17:30:39 +0800

    替换了天气图标

--- app/src/main/java/com/realmax/smarthomeversion2/activity/AirConditioningActivity.java
+++ app/src/main/java/com/realmax/smarthomeversion2/activity/AirConditioningActivity.java
@@ -2,12 +2,38 @@ package com.realmax.smarthomeversion2.activity;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 import com.realmax.smarthomeversion2.R;
 
 public class AirConditioningActivity extends BaseActivity {
+    private RelativeLayout rl_back;
+    private ImageView iv_one;
+    private ImageView iv_two;
+    private ImageView iv_three;
+    private ImageView iv_four;
+    private ImageView iv_weather;
+    private TextView tv_temperature;
+    private TextView tv_setTemperature;
+    private TextView tv_windSpeed;
+    private TextView tv_mode;
+    private CheckBox cb_powerSwitch;
+    private ImageView iv_cold;
+    private ImageView iv_hot;
+    private ImageView iv_plus;
+    private ImageView iv_reduce;
+    private ImageView iv_up;
+    private ImageView iv_down;
+    private ImageView iv_switchLeft;
+    private ImageView iv_switchRight;
+    private TextView tv_currentRoom;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
@@ -20,12 +46,36 @@ public class AirConditioningActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-
+        rl_back = (RelativeLayout) findViewById(R.id.rl_back);
+        iv_one = (ImageView) findViewById(R.id.iv_one);
+        iv_two = (ImageView) findViewById(R.id.iv_two);
+        iv_three = (ImageView) findViewById(R.id.iv_three);
+        iv_four = (ImageView) findViewById(R.id.iv_four);
+        iv_weather = (ImageView) findViewById(R.id.iv_weather);
+        tv_temperature = (TextView) findViewById(R.id.tv_temperature);
+        tv_setTemperature = (TextView) findViewById(R.id.tv_setTemperature);
+        tv_windSpeed = (TextView) findViewById(R.id.tv_windSpeed);
+        tv_mode = (TextView) findViewById(R.id.tv_mode);
+        cb_powerSwitch = (CheckBox) findViewById(R.id.cb_powerSwitch);
+        iv_cold = (ImageView) findViewById(R.id.iv_cold);
+        iv_hot = (ImageView) findViewById(R.id.iv_hot);
+        iv_plus = (ImageView) findViewById(R.id.iv_plus);
+        iv_reduce = (ImageView) findViewById(R.id.iv_reduce);
+        iv_up = (ImageView) findViewById(R.id.iv_up);
+        iv_down = (ImageView) findViewById(R.id.iv_down);
+        iv_switchLeft = (ImageView) findViewById(R.id.iv_switchLeft);
+        iv_switchRight = (ImageView) findViewById(R.id.iv_switchRight);
+        tv_currentRoom = (TextView) findViewById(R.id.tv_currentRoom);
     }
 
     @Override
     protected void initEvent() {
-
+        rl_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/realmax/smarthomeversion2/activity/TransducerActivity.java
+++ app/src/main/java/com/realmax/smarthomeversion2/activity/TransducerActivity.java
@@ -2,12 +2,30 @@ package com.realmax.smarthomeversion2.activity;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 import com.realmax.smarthomeversion2.R;
 
 public class TransducerActivity extends BaseActivity {
+    private RelativeLayout rl_back;
+    private ImageView iv_one;
+    private ImageView iv_two;
+    private ImageView iv_three;
+    private ImageView iv_four;
+    private ImageView iv_weather;
+    private TextView tv_temperature;
+    private TextView tv_humidity;
+    private TextView tv_smoke;
+    private TextView tv_character;
+    private ImageView iv_switchLeft;
+    private ImageView iv_switchRight;
+    private TextView tv_currentRoom;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
@@ -20,12 +38,35 @@ public class TransducerActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-
+        // 返回按钮
+        rl_back = (RelativeLayout) findViewById(R.id.rl_back);
+        // 时间显示
+        iv_one = (ImageView) findViewById(R.id.iv_one);
+        iv_two = (ImageView) findViewById(R.id.iv_two);
+        iv_three = (ImageView) findViewById(R.id.iv_three);
+        iv_four = (ImageView) findViewById(R.id.iv_four);
+        // 天气控件
+        iv_weather = (ImageView) findViewById(R.id.iv_weather);
+        // 传感器状态值
+        tv_temperature = (TextView) findViewById(R.id.tv_temperature);
+        tv_humidity = (TextView) findViewById(R.id.tv_humidity);
+        tv_smoke = (TextView) findViewById(R.id.tv_smoke);
+        tv_character = (TextView) findViewById(R.id.tv_character);
+        // 切换房间
+        iv_switchLeft = (ImageView) findViewById(R.id.iv_switchLeft);
+        iv_switchRight = (ImageView) findViewById(R.id.iv_switchRight);
+        // 当前房间名称
+        tv_currentRoom = (TextView) findViewById(R.id.tv_currentRoom);
     }
 
     @Override
     protected void initEvent() {
-
+        rl_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     @Override
